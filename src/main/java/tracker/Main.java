4a5555d98188238b80a51a703047989d8c20c88c
Anna Vlasova <isebella@mail.ru>
2022-06-20 19:16:37 +0300

    дз 2 - добавлено дз

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -1,7 +1,26 @@
 package tracker;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        String resume;
+        String criticality;
+        int amountDays;
+        final int Length_Week = 5;
+        boolean weekCorrect;
+        System.out.println("Введите резюме дефекта:");
+        Scanner scanner = new Scanner(System.in);
+        resume = scanner.nextLine();
+        System.out.println("Введите критичность дефекта (Highest, high, middle, low):");
+        criticality = scanner.nextLine();
+        System.out.println("Введите ожидамое количество дней на исправление дефекта");
+        amountDays = scanner.nextInt();
+        scanner.nextLine();
+        weekCorrect = Length_Week < amountDays;
+        System.out.println("Информация о дефекте: ");
+        System.out.println("Резюме: " + resume + " | " + "Серьезность " + criticality +
+                " | " + "Количество дней на исправление " + " | " +
+                "Займет больше рабочей недели (true/false): " + weekCorrect);
     }
 }
