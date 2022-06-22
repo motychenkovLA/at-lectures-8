d6e0487ae544900fe884718964438c3b560ddf90
lmotychenkov <motychenkovLA@gmail.com>
2022-06-22 12:10:49 +0300

    дз 3 - замечания

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -4,11 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // todo 3 - тут хранится не сам дефект (резюме, приор, дни), а его строковое представление
+        // todo 3 - 10 это константа
         String [] array = new String[10];
         Scanner scanner = new Scanner(System.in);
+        // todo 0 - называется quit, но при этом пока true программа наоборот не выходит
         boolean quit = true;
+        // todo 0 - не говорящее название
         int i = 0;
         while (quit) {
+            // todo 1 - за 120 символов (белая лингия в идее) лучше не вылезать, за монитор тем более, надо бы перенести
             System.out.println("Главное меню:  \nДобавить новый дефевкт - введите (add) \nВывести список дефектов - введите (list) \nВыйти из программы - введите (quit)");
             switch (scanner.nextLine()) {
                 case "add":
@@ -28,6 +33,7 @@ public class Main {
                     i++;
                     break;
                 case "list":
+                    // todo 3 - выводит кучу null-ов кроме дефектов
                     for(String element:array) {
                         System.out.println(element);
                     }
