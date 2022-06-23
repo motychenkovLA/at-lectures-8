ee211314254fdd3471cc9bff4188ef2830c1d6eb
ykondakova <ykondakova@t1-consulting.ru>
2022-06-23 11:47:40 +0300

    ДЗ 3

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -5,23 +5,71 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+        String[] bugArray = new String[10];
 
-        System.out.println("Введите резюме дефекта");
-        String defectSummary = scanner.nextLine();
+        while (true) {
 
-        System.out.println("Введите критичность дефекта\nСписок вариантов:\nTrivial, Minor, Major, Critical, Blocker");
-        String defectSeverity = scanner.nextLine();
+            System.out.println("Что нужно сделать?\nadd - добавить новый дефект\n" + "list - вывести список дефектов\nquit - выйти из программы");
+            Scanner scanner = new Scanner(System.in);
+            String userChoice = scanner.nextLine();
 
-        System.out.println("Введите ожидаемое количество дней на исправление дефекта");
-        int numberOfDays = scanner.nextInt();
+            if (userChoice.equals("add")) {
 
-        boolean numberOfWorkDays = numberOfDays > 5;
+                boolean addDefectToArray = false;
 
-        String defect = "Резюме: " + defectSummary + "\n" + "Критичность дефекта: " + defectSeverity + "\n" +
-                "Количество дней на исправление: " + numberOfDays + "\n" + "Займет больше рабочей недели: " + numberOfWorkDays;
+                for (int i = 0; i < bugArray.length; i++) {
 
-        System.out.println(defect);
+                    if (bugArray[i] == null) {
+                        System.out.println("Введите резюме дефекта");
+                        String defectSummary = scanner.nextLine();
 
+                        System.out.println("Введите критичность дефекта\nСписок вариантов:\nTrivial, Minor, Major, Critical, Blocker");
+                        String defectSeverity = scanner.nextLine();
+
+                        System.out.println("Введите ожидаемое количество дней на исправление дефекта");
+                        int numberOfDays = scanner.nextInt();
+
+                        bugArray[i] = defectSummary + " | " + defectSeverity + " | " + numberOfDays;
+
+                        addDefectToArray = true;
+
+                        System.out.println("Дефект успешно добавлен" + "\n");
+                        break;
+                    }
+                }
+                if (!addDefectToArray) {
+                    System.out.println("Невозможно добавить более "+bugArray.length+" дефектов"+"\n");
+                }
+            }
+            else if (userChoice.equals("list")) {
+
+                int amountNull = 0;
+
+                for (String defect : bugArray) {
+                    if (defect == null) {
+                        amountNull++;
+                    }
+                }
+                if (amountNull == bugArray.length) {
+                    System.out.println ("В системе нет ни одного дефекта\n");
+                    }
+                else {
+                    System.out.println("Список дефектов:");
+                    for (String defect : bugArray) {
+                        if (defect != null)
+                            System.out.println(defect);
+                    }
+                    System.out.println();
+                }
+            }
+            else if (userChoice.equals("quit")) {
+
+                System.out.println("Выход из программы");
+                break;
+            }
+            else {
+                System.out.println ("Необходимо ввести одно из трех значений!\n");
+            }
+        }
     }
 }
