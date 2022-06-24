66bc33e2c2e12ffccbe91c137db57943a49ceb2b
lmotychenkov <motychenkovLA@gmail.com>
2022-06-24 10:16:04 +0300

    дз 3 - замечания

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        // todo 0 - коментарии дублируют код
+        // todo 1 - код стайл не как у константы, почему 3?
         final int maxDefects = 3; //константа - максимальное количество дефектов
         String[] description = new String[maxDefects];//вводим строковый массив description с количеством элементов, равному максимальному кол-ву дефектов
         String[] severity = new String[maxDefects];//вводим строковый массив severity с количеством элементов, равному максимальному кол-ву дефектов
@@ -35,7 +37,7 @@ public class Main {
                                 "\n" + numberOfDays[count]);
                         count++;//увеличиваем счетчик
                     }
-                    else {
+                    else { // todo 1 - какой-то случайный перенос
                         System.out.println("Закончилось место для заведения дефектов!");
                     }
                     break;
