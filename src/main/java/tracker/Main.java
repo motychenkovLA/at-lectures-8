372268f39b2be0a7af176db7655fa9af0ffbead8
akzhukov <zhykov.a.k@gmail.com>
2022-06-24 09:56:56 +0300

    дз 3 - исправления, добавлены массивы под описание, критичность и дни, исправлена константа для макс.кол-ва дефектов, исправлены переносы и отступы, удалён лишний счетчик

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -1,18 +1,18 @@
 package tracker;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        // todo 3 - 10 это константа
-        // todo 3 - дефекты хранятся в виде строк, хотя строками не являются, дефект состоит из нескольких полей разного тип
-        String[] arrayOfDefects = new String[10]; //вводим строковый массив arrayOfDefects с количеством элементов 10
+        final int maxDefects = 3; //константа - максимальное количество дефектов
+        String[] description = new String[maxDefects];//вводим строковый массив description с количеством элементов, равному максимальному кол-ву дефектов
+        String[] severity = new String[maxDefects];//вводим строковый массив severity с количеством элементов, равному максимальному кол-ву дефектов
+        int[] numberOfDays = new int[maxDefects];//вводим числовой массив numberOfDays с количеством элементов, равному максимальному кол-ву дефектов
         int count = 0; //вводим счетчик
-        final int maxDefects = arrayOfDefects.length; //константа - максимальное количество дефектов, равное длинне массива
-        // todo 0 - не говорящее название
-        boolean flag = true; //флаг завершения цикла
-        while (flag) {
+        boolean work = true; //флаг завершения цикла
+        while (work) {
             System.out.println("Вы находитесь в главном меню багтрекинговой системы. Выберите действие:" +
                     "\nДобавить новый дефект (введите команду add)" +
                     "\nВывести список дефектов (введите команду list)" +
@@ -20,41 +20,34 @@ public class Main {
             String command = scan.nextLine();
             switch (command) {
                 case "add": //ввод дефекта
-                    // todo 0 - переносы { и отступы
-                    if (count < maxDefects ) //проверяем сколько дефектов заведено
-                    {
+                    if (count < maxDefects) {//проверяем сколько дефектов заведено
                         System.out.println("Введите краткое описание дефекта:");
-                        String description = scan.nextLine();
-                        System.out.println("Отлично! Следующий шаг: введите критичность дефекта" +
+                        description[count] = scan.nextLine();
+                        System.out.println("Следующий шаг: введите критичность дефекта" +
                                 "\n(Доступны следующие варианты: Тривиальный, Незначительный, Значительный, Критический, Блокирующий)");
-                        String severity = scan.nextLine();
-
-                        System.out.println("Замечательно! Следующий шаг: введите ожидаемое количество дней на исправление дефекта: ");
-                        int numberOfDays = scan.nextInt();
+                        severity[count] = scan.nextLine();
+                        System.out.println("Следующий шаг: введите ожидаемое количество дней на исправление дефекта: ");
+                        numberOfDays[count] = scan.nextInt();
                         scan.nextLine();
-
-                        System.out.println("Великолепно! Вы ввели следующую информацию по дефекту: " +
-                                "\nОписание дефекта: " + description +
-                                "\nКритичность дефекта: " + severity +
-                                "\nКоличество дней на исправление: " + numberOfDays);
-
-                        arrayOfDefects[count] = description + " " + severity + " " + numberOfDays; //записываем дефект в массив
+                        System.out.println("Вы ввели следующий дефект: " +
+                                "\n" + description[count] +
+                                "\n" + severity[count] +
+                                "\n" + numberOfDays[count]);
                         count++;//увеличиваем счетчик
                     }
                     else {
-                            System.out.println("Закончилось место для заведения дефектов!");
-                        }
+                        System.out.println("Закончилось место для заведения дефектов!");
+                    }
                     break;
                 case "list": //команда список дефектов
                     System.out.println("Список заведённых дефектов (описание, критичность, кол-во дней на исправление):");
-                    int countList = count; //вводим ещё один счетчик, равный count // todo 3 - зачем?
-                    for (int i = 0; i < countList; i++) { //выводим построково дефекты из массива в количестве i, равному счетчику count
-                        System.out.println(arrayOfDefects[i]);
+                    for (int i = 0; i < count; i++) { //выводим построково дефекты из массива в количестве i, равному счетчику count
+                        System.out.println(description[i] + " // " + severity[i] + " // " + numberOfDays[i]);
                     }
                     break;
                 case "quit":
                     System.out.println("Завершение работы программы");
-                    flag = false;
+                    work = false;
                     break;
                 default:
                     System.out.println("Введена некорректная команда, возврат в главное меню");
