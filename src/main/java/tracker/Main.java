c5a71e779847e55da8407b5f98aeda4add8043e4
VasinaEV <evasina@t1-consulting.ru>
2022-06-27 15:38:02 +0300

    дз - 2 исправления

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -5,32 +5,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // todo 0 - название переменной не по код стайлу
-        Scanner Talk = new Scanner(System.in);
+        //изменила на opisanieDefecta
+        Scanner opisanieDefecta = new Scanner(System.in);
         // todo 1 - переменная не используется
+        //заиспользовала
         String bug;
 //process
         System.out.println("Хаюшки! Что у тебя за дефект?");
         // todo 0 - название переменной не по код стайлу
         // todo 3 - лучше использовать nextLine
-        String bug_name = Talk.next();
+        //поменяла на bugName
+        String bugName = opisanieDefecta.nextLine();
         System.out.println("Укажи критичность дефекта :\n" + "high\n" + "medium\n" + "low");
         // todo 0 - название переменной не по код стайлу
-        String bug_level = Talk.next();
+        //поменяла на bugLevel
+        String bugLevel = opisanieDefecta.nextLine();
         System.out.println("Ожидаемый срок исправления в днях:");
         // todo 0 - название переменной не по код стайлу
-        int bug_time = Talk.nextInt();
+        //поменяла на bugTime
+        int bugTime = opisanieDefecta.nextInt();
         // todo 3 - нет переноса курсора чтения после nextInt
-        // todo 3 - займет больше недели надо в ту же строку вывести бы
-        System.out.println(
-                "Ваш дефект:\n" +
-                        bug_name + "\n" +
-                        "Критичность - " + bug_level + "\n" +
-                        "Срок исправления - " + bug_time
-        );
+        opisanieDefecta.nextLine();
         // todo 0 - рабочая неделя 5 дней
         // todo 3 - длина рабочей недели это константа
         // todo 3 - очень сложное преобразование булеана в самого себя
-        boolean weekIsEnough = (bug_time > 7) ? false : true;
-        System.out.println("Хватит недельки - " + weekIsEnough);
+        final int rabochayaNedelya = 5;
+        bug = "Ваш дефект:\n" + bugName + "\n" + "Критичность - " + bugLevel + "\n" + "Срок исправления - " + bugTime + "\n";
+        // todo 3 - займет больше недели надо в ту же строку вывести бы
+        if (bugTime <= rabochayaNedelya) {
+            System.out.println(bug + "Успеем поправить за неделю");
+        } else {
+            System.out.println(bug + "Потребуется больше рабочей недели на исправление");
+        }
     }
 }
