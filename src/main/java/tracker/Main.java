95a66474a6c229103bf58863e8fac65415c60e20
VasinaEV <evasina@t1-consulting.ru>
2022-06-27 23:18:35 +0300

    дз - 2 исправления-2

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -5,25 +5,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // todo 0 - транслит лучше не использовать + это не описание, это сканер
-        Scanner opisanieDefecta = new Scanner(System.in);
-        // todo 1 - объявление оторвано от инициации
-        String bug;
-//process
+        //done
+        Scanner inputText = new Scanner(System.in);
         System.out.println("Хаюшки! Что у тебя за дефект?");
-        String bugName = opisanieDefecta.nextLine();
+        String bugName = inputText.nextLine();
         System.out.println("Укажи критичность дефекта :\n" + "high\n" + "medium\n" + "low");
-        String bugLevel = opisanieDefecta.nextLine();
+        String bugLevel = inputText.nextLine();
         System.out.println("Ожидаемый срок исправления в днях:");
-        int bugTime = opisanieDefecta.nextInt();
-        opisanieDefecta.nextLine();
+        int bugTime = inputText.nextInt();
+        inputText.nextLine();
         // todo 3 - код стайл не как у констант
-        final int rabochayaNedelya = 5;
+        //done
+        final int WORKING_WEEK = 5;
+        // todo 1 - объявление оторвано от инициации
+        //done
+        String bug;
         bug = "Ваш дефект:\n" + bugName + "\n" + "Критичность - " + bugLevel + "\n" + "Срок исправления - " + bugTime + "\n";
         // todo 3 - if не входит во вторую лекцию
-        if (bugTime <= rabochayaNedelya) {
-            System.out.println(bug + "Успеем поправить за неделю");
-        } else {
-            System.out.println(bug + "Потребуется больше рабочей недели на исправление");
-        }
+        //done
+        String enoughTime = bugTime<=5 ? "Успеем поправить за неделю":"Потребуется больше рабочей недели на исправление";
+
+        System.out.println(bug + enoughTime);
     }
 }
