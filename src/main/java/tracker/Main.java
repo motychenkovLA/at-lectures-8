c384d1d11db484c92d1f1605bef1a5147809aa3c
VasinaEV <evasina@t1-consulting.ru>
2022-06-28 23:55:55 +0300

    дз-2 исправления

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -5,17 +5,66 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner inputText = new Scanner(System.in);
-        System.out.println("Хаюшки! Что у тебя за дефект?");
-        String bugName = inputText.nextLine();
-        System.out.println("Укажи критичность дефекта :\n" + "high\n" + "medium\n" + "low");
-        String bugLevel = inputText.nextLine();
-        System.out.println("Ожидаемый срок исправления в днях:");
-        int bugTime = inputText.nextInt();
-        inputText.nextLine();
-        final int WORKING_WEEK = 5;
-        String bug = "Ваш дефект:\n" + bugName + "\n" + "Критичность - " + bugLevel + "\n"
-                + "Срок исправления - " + bugTime + "\n";
-        boolean enoughTime = bugTime <= WORKING_WEEK;
-        System.out.println(bug + "Будет исправлено в течение рабочей недели (5 дн) - " + enoughTime);
+        String[] bugs = new String[10];
+        boolean cycle = true;
+        int i = 0;
+        do {
+            System.out.println("---Главное меню---");
+            String menu = "Выберете команду:\n " +
+                    "- добавить новый дефект (\"add\")\n " +
+                    "- вывести список (\"list\")\n " +
+                    "- выйти из программы (\"quit\") - главное меню";
+            System.out.println(menu);
+            String Command = inputText.nextLine();
+            switch (Command) {
+                //добавить дефект
+                case "add":
+                    if (i < 10) {
+                        //process
+                        System.out.println("Хаюшки! Что у тебя за дефект?");
+                        String bugName = inputText.nextLine();
+                        System.out.println("Укажи критичность дефекта :\n" + "high\n" + "medium\n"
+                                + "low");
+                        String bugLevel = inputText.nextLine();
+                        System.out.println("Ожидаемый срок исправления в днях:");
+                        int bugTime = inputText.nextInt();
+                        inputText.nextLine();
+                        final int WORKING_WEEK = 5;
+                        int bugNumber = i + 1;
+                        String bug = "Дефект №" + bugNumber + ":\n" + bugName + "\n" +
+                                "Критичность - " + bugLevel + "\n" + "Срок исправления - " +
+                                bugTime + "\n";
+                        boolean enoughTime = bugTime <= WORKING_WEEK;
+                        System.out.println(bug +
+                                "Будет исправлено в течение рабочей недели (5 дн) - " +
+                                enoughTime);
+                        bugs[i] = bug;
+                        i++;
+                        String exit = "quit";
+                        String addedText;
+                        do {
+                            System.out.println("Выйдите в главное меню с помощью команды - quit");
+                            addedText = inputText.next();
+                        } while (!addedText.equals(exit));
+                    } else {
+                        //ошибка
+                        System.out.println("Хватит дефектов!\n");
+                    }
+                    break;
+                case "list":
+                    if (i == 0) {
+                        System.out.println("Пока нет дефектов");
+                    } else {
+                        System.out.println("Список дефектов:");
+                        for (int j = 0; j < i; j++) {
+                            System.out.println(bugs[j]);
+                        }
+                        break;
+                    }
+                    //выход
+                case "quit":
+                    break;
+            }
+        } while (cycle);
     }
 }
