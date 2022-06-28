8a936a30b524d954f3c562de39c96ab0a63d9b7c
lmotychenkov <motychenkovLA@gmail.com>
2022-06-28 10:29:11 +0300

    дз 2 - замечания

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -4,8 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // todo 0 - транслит лучше не использовать + это не описание, это сканер
-        //done
         Scanner inputText = new Scanner(System.in);
         System.out.println("Хаюшки! Что у тебя за дефект?");
         String bugName = inputText.nextLine();
@@ -14,15 +12,13 @@ public class Main {
         System.out.println("Ожидаемый срок исправления в днях:");
         int bugTime = inputText.nextInt();
         inputText.nextLine();
-        // todo 3 - код стайл не как у констант
-        //done
+        // todo 3 - не используется
         final int WORKING_WEEK = 5;
-        // todo 1 - объявление оторвано от инициации
-        //done
+        // todo 1 - объявление оторвано от инициации, +1
         String bug;
+        // todo 0 - желательно не вылезать за белую полосу (120 символов) / край экрана
         bug = "Ваш дефект:\n" + bugName + "\n" + "Критичность - " + bugLevel + "\n" + "Срок исправления - " + bugTime + "\n";
-        // todo 3 - if не входит во вторую лекцию
-        //done
+        // todo 3 - ТЗ: "займет больше рабочей недели (true/false)", нужно выводить boolean, а не String
         String enoughTime = bugTime<=5 ? "Успеем поправить за неделю":"Потребуется больше рабочей недели на исправление";
 
         System.out.println(bug + enoughTime);
