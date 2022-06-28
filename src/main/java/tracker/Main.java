d16f577ac5c680a08a4ff0e953fe7cc4e7dd27bd
vandreeva <vaandre27@outlook.com>
2022-06-28 17:05:57 +0300

    ДЗ 3.

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -4,23 +4,54 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Введите резюме дефекта.");
-        Scanner scan = new Scanner (System.in);
-        String resume = scan.nextLine();
+        Scanner scan = new Scanner(System.in);
+        boolean run = true;
+        final int countOfDefects = 2; //максимальное число хранимых дефектов в программе
+        String[] resume = new String[countOfDefects]; //массив резюме дефекта
+        String [] severity = new String[countOfDefects]; //массив критичности
+        int [] days = new int[countOfDefects]; //массив кол-во дней на исправление
+        int count = 0;
 
-        //ввод критичности дефекта с выбором.
-        System.out.println("Введите критичность дефекта. \n Список вариантов: \n -Тривиальный \n -Незначительный \n -Значительный \n -Критический \n -Блокирующий");
-        //System.out.print("Резюме дефекта: " + resume);
-        String severity = scan.nextLine();
+        while (run) {
+            System.out.println("Введите команду для выбора действия. \n add - Добавить новый дефект." +
+                    " \n list - Вывод списка дефектов. \n quit - Выход.");
+            String choice = scan.nextLine();
 
-        System.out.println("Введите ожидаемое количество дней на исправление дефекта.");
-        //System.out.print("Резюме дефекта: " + resume);
-        int days = scan.nextInt();
+            switch (choice) {
+                case "add":
+                    if (count > (countOfDefects - 1) ) {
+                        System.out.print("Заведено максимальное количество дефектов. ");
+                    }
+                    System.out.println("Введите резюме дефекта:");
+                    resume[count] = scan.nextLine();
 
-        final int workingWeek=5;
-        boolean week = workingWeek < days;
+                    System.out.println("Введите критичность дефекта. \n Список вариантов: \n 1 - Тривиальный " +
+                            "\n 2 - Незначительный \n 3 - Значительный \n 4 - Критический \n 5 - Блокирующий");
+                    severity[count] = scan.nextLine();
 
-        System.out.print("Резюме дефекта: " + resume + "|" + "Критичность: " + severity + "|" + "количество дней на исправление: " + days + "|" + "займет больше рабочей недели: " + week);
+                    System.out.println("Введите ожидаемое количество дней на исправление дефекта.");
+                    days[count] = scan.nextInt();
+                    scan.nextLine();
 
+                    count++;
+                    break;
+
+                case "list":
+                   for (int j = 0; j < countOfDefects; j++) {
+                     System.out.print("Резюме дефекта: " + resume[j] + " | " + "Критичность: " + severity[j] +
+                             " | " + "количество дней на исправление: " + days[j] + "\n");
+                    }
+                    break;
+
+                case "quit":
+                    run = false;
+                    System.out.print("Вы вышли из главного меню. ");
+                    break;
+
+                default:
+                    System.out.print("Неверно введена команда. ");
+                    break;
+            }
+        }
     }
 }
