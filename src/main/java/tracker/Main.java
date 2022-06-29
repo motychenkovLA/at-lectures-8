10a619fe309ab7814c9847a4f58b8cab9ef67006
Alena <zaharova_aa@icloud.com>
2022-06-29 21:53:46 +0600

    Выполнила 3 ДЗ

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -3,22 +3,40 @@ package tracker;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.println("введите резюме деффекта");
-        String  resume = s.nextLine();
-        System.out.println("введите критичность деффекта");
-        System.out.println("блокирующий, критический, незначительный");
-        String  critical = s.nextLine();
-        System.out.println("введите ожидаемое количество дней на исправление");
-        int days = Integer.parseInt(s.nextLine()); // преобразуем кол-во дней из строкового типа в целоцисленный
-        String daysOut;
-        if (days > 5) {
-            daysOut = "займет больше рабочей недели";
-        } else {
-            daysOut = "займет меньше рабочей недели";
-        }
-        System.out.println(resume + " | " + critical + " | " + days + " | " + daysOut );
+        Scanner scanner = new Scanner(System.in);
+        String [] bugs = new String[10];
+        int bugsAmount = 0;
+
+        do {
+            System.out.println();
+            System.out.println("-- Главное меню --");
+            System.out.println("Добавьте новый деффект(add), посмотрите список дефектов(list) или выйдите из программы(quit):");
+            String userInput = scanner.nextLine();
+
+            if (userInput.equals("add")) { // Проверяем что юзер ввёл: add
+                // Обрабатываем add
+                if (bugsAmount < 10) {
+                    System.out.println("Введите имя деффекта:");
+                    String userInput2 = scanner.nextLine();
+                    bugs[bugsAmount] = userInput2;
+                    bugsAmount++;
+                } else {
+                    System.out.println("Слишком много багов!");
+                }
+            } else if (userInput.equals("list")) { // Проверяем что юзер ввёл: list
+                // Обрабатываем List
+                for (String bug: bugs) { // Проходим по массиву bugs для вывода багов
+                    if (bug != null) { // Проверяем, если баг есть в текущем индексе, то выводим его
+                        System.out.println(bug);
+                        System.out.println();
+                    }
+                }
+            } else if (userInput.equals("quit")) { // Проверяем что юзер ввёл: quit
+                // Обрабатываем Quit
+                break;
+            }
+        } while (true);
     }
 }
+
