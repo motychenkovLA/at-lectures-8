621cfb1164a23a0fb126efb9ab54045890e312f0
FeoktistovI <feoxis@gmail.com>
2022-06-30 18:05:45 +0300

    ДЗ 3

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -5,20 +5,45 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Введите резюме дефекта");
-        String rezume1 = scanner.nextLine();
+        final int counter_defect = 10;
+        String[] rezume1 = new String[counter_defect];
+        String[] critical1 = new String[counter_defect];
+        int[] workdays = new int[counter_defect];
+        Scanner sc = new Scanner(System.in);
+        boolean run = true;
+        int count1 = 0;
+        while (run) {
 
-        System.out.println("Введите критичность дефекта из списка High|Medium|Low");
-        String critical1 = scanner.nextLine();
+            System.out.println("Главное меню:" + " Добавить новый дефект (add)" + "|" + "Вывести список дефектов (list)" + "|" + "Выйти из программы (quit)" + "|");
+            switch (sc.nextLine()) {
 
-        System.out.println("Введите количество дней на исправление");
-        int days1 = scanner.nextInt();
+                case "add":
+                    if(count1 >= counter_defect) {
+                        System.out.println("Достигнуто максимальное количество дефектов");
+                        System.out.println();
+                        break;
+                    }
+                    System.out.println("Введите резюме дефекта");
+                    rezume1[count1] = sc.nextLine();
+                    System.out.println("Введите критичность дефекта из списка High|Medium|Low");
+                    critical1[count1] = sc.nextLine();
+                    System.out.println("Введите количество дней на исправление");
+                    workdays[count1] = sc.nextInt();
+                    sc.nextLine();
+                    count1++;
+                    break;
 
-        boolean workdays = days1 > 5;
+                case "list":
+                    for(int a = 0;a < count1;a++) {
+                        System.out.println(rezume1[a] + "|" + critical1[a] + "|" +workdays[a]);
+                    }
+                    break;
 
-        System.out.print("|" + "Резюме:"+ rezume1 +"|"+"Серьезность:" + critical1 +"|" + "Количество дней на исправление:" + days1 +"|" + "Займет больше рабочей недели (true/false):" + workdays + "|");
-
+                case "quit":
+                    run = false;
+                    break;
+            }
+        }
 
     }
 }
