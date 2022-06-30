58db70b2b9dda0a1b02a1c30611e402ebf998c87
FeoktistovI <feoxis@gmail.com>
2022-06-30 16:46:55 +0300

    ДЗ 2 - Исправление

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -1,7 +1,26 @@
 package tracker;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!!!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите резюме дефекта");
+        String rezume1 = scanner.nextLine();
+
+        System.out.println("Введите критичность дефекта из списка High|Medium|Low");
+        String critical1 = scanner.nextLine();
+
+        System.out.println("Введите количество дней на исправление");
+        int days1 = scanner.nextInt();
+
+        boolean workdays = days1 > 5;
+
+        System.out.print("|" + "Резюме:"+ rezume1 +"|"+"Серьезность:" + critical1 +"|" + "Количество дней на исправление:" + days1 +"|" + "Займет больше рабочей недели (true/false):" + workdays + "|");
+
+
     }
 }
+
+
