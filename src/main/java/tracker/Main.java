f7a2b2e2dfd675efbc1cca86d0959f5755d2b638
VasinaEV <evasina@t1-consulting.ru>
2022-07-01 14:10:39 +0300

    дз-3 исправления

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -64,6 +64,7 @@ public class Main {
                 case "quit":
                         active = false;
                         System.out.println("Bye-bye :)");
+                        break;
         }
     }
 }
