5cd0dbff085f67598fccfd243971d25a253ba8fd
FeoktistovI <feoxis@gmail.com>
2022-08-03 21:01:51 +0300

    ДЗ 4-7

---
+++ src/main/java/tracker/DefectStatus.java
@@ -0,0 +1,9 @@
+package tracker;
+
+public enum DefectStatus {
+    OPEN,
+    ASSIGNED,
+    FIXED,
+    VERIFIED,
+    CLOSED;
+}
---
+++ src/main/java/tracker/Repository.java
@@ -0,0 +1,39 @@
+package tracker;
+
+public class Repository {
+    private Defect[] arrayDefects;
+    private int amountDefects = 0;
+    private int amount;
+
+    public void add(Defect defect) {
+        if (amountDefects < arrayDefects.length) {
+            arrayDefects[amountDefects] = defect;
+            amountDefects++;
+        } else {
+            System.out.println("Невозможно ввести новый дефект, память заполнена");
+        }
+    }
+
+    public Defect[] getAll() {
+        int lengthArrayForReturn = amountDefects;
+        Defect[] defectsForReturn = new Defect[lengthArrayForReturn];
+        System.arraycopy(arrayDefects, 0, defectsForReturn, 0, lengthArrayForReturn);
+        return defectsForReturn;
+    }
+
+    public int getAmountDefects() {
+        return amountDefects;
+    }
+
+    public Defect[] getArrayDefects() {
+        return arrayDefects;
+    }
+
+    public Repository(int amount) {
+        this.amount = amount;
+        arrayDefects = new Defect[amount];
+    }
+
+    public Repository() {
+    }
+}
