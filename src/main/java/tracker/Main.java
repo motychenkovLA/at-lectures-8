2ccdacedf72278c9b92731df2cb6f702e49e7287
Anna Vlasova <isebella@mail.ru>
2022-08-05 19:32:09 +0300

    дз 8 - реализованы методы equals(...) и hashCode() у Defect и Attachment-ов. Добавлена обработка невалидных данных введённых пользователем

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -1,26 +1,29 @@
 package tracker;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Scanner scanner = new Scanner(System.in);
         Repository repository = new Repository(3);
         //переменная отвечающая за работу цикла
+        Criticality criticality;
+        int amountDays;
+        Attachment attachment;
         boolean isRun = true;
-        //цикл для выбора действия пользователя
+
         try(Scanner scanner = new Scanner(System.in);) {
-            MenuForInn menuForInn = new MenuForInn();
+            //цикл для выбора действия пользователя
         while (isRun) {
 
-                String userDo = menuForInn.doIn();
+                String userDo = doIn(scanner);
                 //условный оператор с условием добавления дефекта
                 if (userDo.equals("add")) {
-                    String resume = menuForInn.resumeDefectIn();
-                    Criticality criticality = menuForInn.criticalityDefectIn();
-                    int amountDay = menuForInn.amountDayIn();
-                    Attachment attachment = menuForInn.typeAttachmentIn();
+                    String resume = resumeDefectIn(scanner);
+                    criticality = criticalityDefectIn(scanner);
+                    int amountDay = amountDayIn(scanner);
+                    attachment = typeAttachmentIn(scanner);
                     //объявляем экзмепляр класса Defect
                     Defect defect = new Defect(resume, criticality, amountDay, attachment);
                     //выводим информацию о дефекте
@@ -71,5 +74,80 @@ public class Main {
             System.out.println("Вы ввели неправильное значение. Пожалуйста, начните сначала.");
         }
     }
+
+    public static String doIn(Scanner scanner) {
+        System.out.println("Выберите действие: добавить новый дефект (\"add\"), " +
+                "вывести список (\"list\"), " + "изменить статус (\"change\"), " + "выйти из программы (\"quit\") - главное меню");
+        //объявляем и вводим переменную действия пользователя
+        String userDo = scanner.nextLine();
+        return userDo;
+    }
+
+    public static String resumeDefectIn(Scanner scanner) {
+        System.out.println("Введите резюме дефекта");
+        String resume = scanner.nextLine();
+        return resume;
+    }
+
+    public static Criticality criticalityDefectIn(Scanner scanner) {
+        Criticality criticality = null;
+        System.out.println("Введите критичность дефекта: " +
+                Arrays.toString(Criticality.values()));
+        while (true) {
+            try {
+                String criticalityIn = scanner.nextLine();
+                criticality = Criticality.valueOf(criticalityIn);
+            }catch (IllegalArgumentException e){
+                System.out.println("Введите критичность корректно");
+            }
+            if (criticality != null) {break;}
+        }
+        return criticality;
+    }
+
+    public static int amountDayIn(Scanner scanner) {
+        int amountDays;
+        System.out.println("Введите ожидамое количество дней на исправление дефекта");
+        while (true) {
+            try {
+                amountDays = scanner.nextInt();
+            }catch (IllegalArgumentException | InputMismatchException e){
+                amountDays = -1;
+            }
+            scanner.nextLine();
+            if (amountDays > 0) {break;}
+            else {
+                System.out.println("Введите корректное ожидамое количество дней на исправление дефекта ещё раз");
+            }
+        }
+        return amountDays;
+    }
+
+    public static Attachment typeAttachmentIn(Scanner scanner) {
+        Attachment attachment = null;
+        System.out.println("Выберите тип вложения: \"Комментарий\" (comment) или " +
+                "\"Ссылка на другой дефект\" (link)");
+        boolean isRun = true;
+        while (isRun) {
+            String typeAttachment = scanner.nextLine();
+            if (typeAttachment.equals("comment")) {
+                System.out.println("Введите комментарий:");
+                String comment = scanner.nextLine();
+                attachment = new CommentAttachment(comment);
+                isRun = false;
+            } else if (typeAttachment.equals("link")) {
+                System.out.println("Введите id дефекта:");
+                int idDefect = scanner.nextInt();
+                scanner.nextLine();
+                attachment = new DefectAttachment(idDefect);
+                isRun = false;
+            } else {
+                System.out.println("Введите корректное значение вложения");
+            }
+        }
+        return attachment;
+    }
+
+
 }
 
