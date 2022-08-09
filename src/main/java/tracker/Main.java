6cd76e1db9b584c120101fb0676616f7ba70321a
avmartynov <seven37@yandex.ru>
2022-08-09 12:50:10 +0300

    дз 8 - Исправления

--- src/main/java/tracker/Main.java
+++ src/main/java/tracker/Main.java
@@ -86,41 +86,36 @@ public class Main {
                         break;
                     case "change":
                         System.out.println("Введите id дефекта:");
-                        long id = 0;
-                        while (id <= 0) {
+                        boolean checkId = true;
+                        while (checkId) {
                             try {
-                                id = Long.parseLong(scanner.nextLine());
+                                long id = Long.parseLong(scanner.nextLine());
                                 if (repository.getId(id) == null) {
-                                    System.out.println("Введенный id отсутствует в списке дефектов."+
+                                    System.out.println("Введенный id отсутствует в списке дефектов." +
                                             "\nВведите id дефекта:");
-                                }
-
-                            } catch (NumberFormatException e) {
-                                System.out.println("Введенный id отсутствует в списке дефектов." +
-                                        "\nВведите id дефекта:");
-                            }
-                        }
-
-                        System.out.println("Измените статус дефекта на один из вариантов: ");
-                        Status[] valuesStatus = Status.values();
-                        for (Status value : valuesStatus) {
-                            System.out.println(value);
-                        }
-                        Status status = null;
-                        while (status == null) {
-                            try {
-                                status = Status.valueOf(scanner.nextLine());
-                                for (Defect repo : repository.getAll()) {
-                                    if (id == repo.getId()) {
-                                        repo.setStatus(status);
-                                    }
-                                }
-                            } catch (IllegalArgumentException e) {
+                                    continue;
+                                } else checkId = false;
                                 System.out.println("Измените статус дефекта на один из вариантов: ");
-                                valuesStatus = Status.values();
+                                Status[] valuesStatus = Status.values();
                                 for (Status value : valuesStatus) {
                                     System.out.println(value);
                                 }
+                                Status status = null;
+                                while (status == null) {
+                                    try {
+                                        status = Status.valueOf(scanner.nextLine());
+                                    } catch (IllegalArgumentException e) {
+                                        System.out.println("Измените статус дефекта на один из вариантов: ");
+                                        valuesStatus = Status.values();
+                                        for (Status value : valuesStatus) {
+                                            System.out.println(value);
+                                        }
+                                    }
+                                }
+                                repository.getId(id).setStatus(status);
+                            } catch (NumberFormatException e) {
+                                System.out.println("Введенный id отсутствует в списке дефектов." +
+                                        "\nВведите id дефекта:");
                             }
                         }
                         break;
